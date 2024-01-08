325c54edac4534ca6c512b62c993f3743faa15d6
Brad Bell <brad.bb.bell@gmail.com>
2024-01-08 07:32:08 -0700

    repo check

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <h1 class="text-red text-2xl text-center font-bold">Last Best Ski</h1>
+    <p>Reviving this ol boi!</p>
   </div>
   <RouterView />
 </template>
